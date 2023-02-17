1b8f77c4c5638e8f255b84f6f6bdd8ec0b19d07e
remy-mugisha <mugisharemy2019@gmail.com>
2023-02-17 11:12:32 +0200

    changes the server

--- swagger/swagger-doc.js
+++ swagger/swagger-doc.js
@@ -19,7 +19,7 @@ const options = {
         description: 'Development server',
       },
       {
-        url: 'https://expensive-jade-pants.cyclic.app/',
+        url: 'https://documentation-gt6q.onrender.com/',
         description: 'Production server',
       },
     ],
